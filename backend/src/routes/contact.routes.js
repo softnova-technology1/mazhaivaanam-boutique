98ab2fc30fb1b4f7c254950fa0c342307f4232d6
Akileswaran-Dev <akileswaran.dev@gmail.com>
2026-08-27 11:57:06 +0530

    code split and delete

--- backend/src/routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { submitInquiry } from '../controllers/contact.controller.js';
-
-const router = Router();
-
-router.post('/', submitInquiry);
-
-export default router;
--- backend/src/utils/generateOrderId.js
+++
@@ -1,9 +0,0 @@
-/**
- * Generate a unique order ID in the format MV-XXXXXX
- */
-const generateOrderId = () => {
-  const num = Math.floor(100000 + Math.random() * 900000);
-  return `MV-${num}`;
-};
-
-export default generateOrderId;
